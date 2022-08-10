bb5c02dc67888b6f2032fbeee474a57207aaaa28
Pavani1524 <boga.pavani09@gmail.com>
2022-08-10 18:39:44 +0530

    details/employee-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminhomeComponent } from './adminhome/adminhome.component';
 import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
  path:'forgotpassword',
  component:ForgotpasswordComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
   },
   {
     path: 'devices',
